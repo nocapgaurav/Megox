c7a3ddb9ca3a5621e7881a7984c34429acc1f7f3
nocapgaurav <gauravvpandita@gmail.com>
2026-01-16 17:15:37 +0530

    development

---
+++ main.py
@@ -0,0 +1,16 @@
+import eel
+import os
+import time
+
+eel.init('www')
+
+# Start backend only (do NOT open chrome automatically)
+eel.start('index.html', host='localhost', port=8000, block=False, mode=None)
+
+time.sleep(1)
+
+# Open Chrome App Window manually
+os.system('open -na "Google Chrome" --args --app="http://localhost:8000/index.html"')
+
+while True:
+    eel.sleep(1)
